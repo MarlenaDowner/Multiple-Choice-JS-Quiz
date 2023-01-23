870f9ff83c1194cb6a96392f7230f19f9907022d
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-01-23 16:33:13 +0000

    added get questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,7 @@
 //varibles for quiz
 
 let currentQuestionIndex = 0;
-let time = question.length * 10;
+let time = question.length * 13;
 let timerID;
 
 //HTML elements: link HTML elements to the functions needed
@@ -24,6 +24,28 @@ let sfxWrong = new Audio("assets/sfx/incorrect.wav");
 // function startQuiz will need a get elementById, link the HTML element and the set an atrribute class hide- this is because the CSS is hiding it 
  
 function getQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+
+    let titleElement = document.getElementById("question-title");
+
+    titleElement.textContent = currentQuestion.title;
+
+    choicesElement.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, index) {
+        let choiceButton = document.createElement("button");
+
+        choiceButton.setAttribute("class", "choice");
+        choiceButton.setAttribute("value", choice);
+
+        choiceButton.textContent = `${index + 1}. ${choice}`
+
+        choiceButton.addEventListener("click", questionClick);
+
+        choicesElement.append(choiceButton);
+    }
+    
+    )
     
 }
 
@@ -31,17 +53,48 @@ function questionClick(){
 
 }
 
-function startQuiz(){
-    alert("hi")
-}
-
 function quizEnd(){
+    clearInterval(timerID);
 
+    let endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class");
+
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time;
+
+    questionsElement.setAttribute("class", "hide");
 }
+
 
 function clockTick(){
+    time--;
+    timerElement.textContent = time;
+
+    if (time <= 0){
+        quizEnd();
+    }
 
 }
+
+
+
+
+function startQuiz(){
+    let startScreenElement = document.getElementById("start-screen");
+    
+    startScreenElement.setAttribute("class", "hide");
+
+    questionsElement.removeAttribute("class");
+
+    timerID = setInterval(clockTick, 1000);
+
+    timerElement.textContent = time;
+
+    getQuestion();
+    
+}
+
+
 
 
 function saveHighScore(){
@@ -56,9 +109,10 @@ function checkForEnter(event){
 ///----------------------------------------------------------------------
 //adds the event listers because there is a button that needs to be clicked
 
+//start button
 //submit button
 //Enter initials
-//start button
+
 
 startButton.addEventListener("click", startQuiz);
 
